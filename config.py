2d4786c6525c1316963250ff39bc345573b77c37
alexfdealmeida <alexferreiradealmeida@gmail.com>
2025-12-10 23:44:12 -0300

    Branch: master; config.py: Fixed path "temp/data/movilens".

--- config.py
+++ config.py
@@ -4,12 +4,15 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 TEMP_DIR = os.path.join(BASE_DIR, 'temp')
 REDIAL_DATA_PATH = os.path.join(TEMP_DIR, 'data', 'redial')
+ML_DATA_PATH = os.path.join(TEMP_DIR, 'data', 'movielens') 
 MODELS_PATH = os.path.join(TEMP_DIR, 'models')
 
 # if not os.path.exists(REDIAL_DATA_PATH):
 #     os.makedirs(REDIAL_DATA_PATH, exist_ok=True)
 # if not os.path.exists(MODELS_PATH):
 #     os.makedirs(MODELS_PATH, exist_ok=True)
+# if not os.path.exists(ML_DATA_PATH):
+#     os.makedirs(ML_DATA_PATH, exist_ok=True)
 
 AUTOREC_MODEL = os.path.join(MODELS_PATH, "autorec")
 SENTIMENT_ANALYSIS_MODEL = os.path.join(MODELS_PATH, 'sentiment_analysis')
@@ -23,11 +26,10 @@ MOVIE_PATH = os.path.join(REDIAL_DATA_PATH, "movies_merged.csv")
 VOCAB_PATH = os.path.join(REDIAL_DATA_PATH, "vocabulary.p")
 
 # Movielens Settings
-ML_DATA_PATH = os.path.join(REDIAL_DATA_PATH, "movielens")
 ML_SPLIT_PATHS = [os.path.join(ML_DATA_PATH, f"split{i}") for i in range(5)]
-ML_TRAIN_PATH = "train_ratings"
-ML_VALID_PATH = "valid_ratings"
-ML_TEST_PATH = "test_ratings"
+ML_TRAIN_PATH = "train_data" 
+ML_VALID_PATH = "valid_data"
+ML_TEST_PATH = "test_data"
 
 # Reddit (Opcional)
 REDDIT_PATH = os.path.join(REDIAL_DATA_PATH, "reddit")
@@ -38,7 +40,7 @@ REDDIT_TEST_PATH = "task4_reddit_test.txt"
 # BERT Settings
 # Large: all-mpnet-base-v2 | 768
 # Small: all-MiniLM-L6-v2 | 384
-BERT_MODEL_NAME = 'all-MiniLM-L6-v2'
+BERT_MODEL_NAME = 'all-MiniLM-L6-v2' 
 BERT_EMBEDDING_SIZE = 384
 
 CONVERSATION_LENGTH_LIMIT = 40
